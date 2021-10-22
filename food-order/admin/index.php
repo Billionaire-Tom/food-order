6b7dd3376b8b101bdcf21d24c14f6eaddae47ff5
Billionaire-Tom <76765481+Billionaire-Tom@users.noreply.github.com>
2021-10-22 18:17:40 +0800

    Add files via upload

---
+++ food-order/admin/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Food Order Website - Home Page</title>
+	<link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+	<!-- menu section starts here -->
+	<div class="menu">
+		<div class="wrapper">
+			Menu
+		</div>
+	</div>
+	<!-- menu section ends here -->
+	
+	<!-- main content section starts here -->
+	<div class="main-content">
+		<div class="wrapper">
+			Main Content
+		</div>
+	
+	</div>
+	<!-- main content section ends here -->
+	
+	<!-- footer section starts here -->
+	<div class="menu">
+		<div class="wrapper">
+			Footer
+		</div>
+	
+	</div>
+	<!-- footer section ends here -->
+</body>
+</html>
